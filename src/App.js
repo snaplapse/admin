257eca5783b42b55f482638caea815e4f5b9883c
Gautham Erathasari <gauthamravie@gmail.com>
2023-02-16 12:14:58 -0500

    Data can now be fetched from Django backend

--- src/App.js
+++ src/App.js
@@ -1,18 +1,20 @@
 import React from 'react';
-import { Admin, Resource, ListGuesser } from 'react-admin';
-import jsonServerProvider from 'ra-data-json-server';
+import { Admin, Resource } from 'react-admin';
+import drfProvider from 'ra-data-django-rest-framework';
 import authProvider from './authProvider';
 import Dashboard from './Dashboard';
 import UserIcon from '@mui/icons-material/Group';
+import { UserList } from './users';
 
-const dataProvider = jsonServerProvider('https://jsonplaceholder.typicode.com');
+const dataProvider = drfProvider('http://localhost:8000/api');
+console.log(dataProvider)
 
 function App() {
   return (
     <Admin dataProvider={dataProvider} authProvider={authProvider} dashboard={Dashboard}>
       <Resource
         name="users"
-        list={ListGuesser}
+        list={UserList}
         icon={UserIcon}
       />
     </Admin>
