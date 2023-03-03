728c27fcda1d87091d8c35a3938b7061abd31dbe
Gautham Erathasari <gauthamravie@gmail.com>
2023-03-02 19:31:16 -0500

    Updated dashboard

--- src/Dashboard.js
+++ src/Dashboard.js
@@ -1,9 +1,12 @@
 import * as React from "react";
 import { Card, CardContent, CardHeader } from '@mui/material';
+import DateTime from "./dateTime";
 
 export default () => (
     <Card>
         <CardHeader title="Welcome to the SnapLapse administration portal!" />
-        <CardContent>AmongBots ftw!!!</CardContent>
+        <CardContent>
+            <DateTime></DateTime>
+        </CardContent>
     </Card>
 );
